3834077611c8d08747bc30ee6a99183367776be9
Henrique Soares da Silva <henrique.silva533@etec.sp.gov.br>
2018-08-23 20:02:38 -0300

    Atualizações

--- TesteXaml/MainWindow.xaml.cs
+++ TesteXaml/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
 
 namespace TesteXaml
 {
@@ -23,6 +25,25 @@ namespace TesteXaml
         public MainWindow()
         {
             InitializeComponent();
+
+            PointLabel = chartPoint =>
+                string.Format("{0} ({1:P})", chartPoint.Y, chartPoint.Participation);
+
+            DataContext = this;
+        }
+
+        public Func<ChartPoint, string> PointLabel { get; set; }
+
+        private void Chart_OnDataClick(object sender, ChartPoint chartpoint)
+        {
+            var chart = (LiveCharts.Wpf.PieChart) chartpoint.ChartView;
+            
+            //clear selected slice.
+            foreach (PieSeries series in chart.Series)
+                series.PushOut = 0;
+
+            var selectedSeries = (PieSeries) chartpoint.SeriesView;
+            selectedSeries.PushOut = 8;
         }
     }
 }
